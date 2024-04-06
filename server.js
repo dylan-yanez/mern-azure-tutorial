7d51272a8fbe41e6bfdd13f182e61deef1c3896b
FootballerMMA <157654156+dylan-yanez@users.noreply.github.com>
2024-04-06 01:47:22 -0500

    axios included
    axios

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const axios = require("axios");
 const port = process.env.PORT || 3001;
 const path = require('path');
 
