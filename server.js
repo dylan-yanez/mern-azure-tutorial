240ee2de766b1b4e80e0d15729cecb0431b4495c
FootballerMMA <157654156+dylan-yanez@users.noreply.github.com>
2024-04-06 00:38:48 -0500

    uodate

--- server.js
+++ server.js
@@ -10,39 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Database is connected..."))
-  .catch((err) => console.log(err));
-
-//db schema
-const userSchema = mongoose.Schema({
-  name: String,
-  lastName: String,
-});
-
-//db model
-const User = new mongoose.model("User", userSchema);
-
-app.get("/get-users", (req, res) => {
-  User.find()
-    .then((users) => res.json(users))
-    .catch((err) => console.log(err));
-});
-
-app.post("/create", (req, res) => {
-  //save to mongodb and send response
-  const newUser = new User({
-    name: req.body.name,
-    lastName: req.body.lastName,
-  });
-
-  newUser
-    .save()
-    .then((user) => res.json(user))
-    .catch((err) => console.log(err));
-});
-
 app.use(express.static("./client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
