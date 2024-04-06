43ed259406a0e785fe3a5dd78b785040090775b1
FootballerMMA <157654156+dylan-yanez@users.noreply.github.com>
2024-04-06 01:44:38 -0500

    check

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/videos", async (req, res) => {
+  try {
+    const { query } = req.query;
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?key=AIzaSyDW5Hsh46Vm10dY-IvCDLBtpBZaPNi30Q4&part=snippet&type=video&q=${query}&maxResults=15`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching videos:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.use(express.static("./client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
