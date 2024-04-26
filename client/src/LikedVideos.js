e8e977070b908ba0158060f1d4bddb3ad66c5f0a
FootballerMMA <157654156+dylan-yanez@users.noreply.github.com>
2024-04-26 15:55:20 -0500

    edit to parameters

--- client/src/LikedVideos.js
+++ client/src/LikedVideos.js
@@ -25,16 +25,16 @@ const LikedVideos = () => {
         {likedVideos.map((video) => (
           <div key={video.id} className="video-card">
             <Link
-              to={`/video/${video.id}?title=${encodeURIComponent(video.title)}&thumbnail=${encodeURIComponent(`https://i.ytimg.com/vi/${video.id}/mqdefault.jpg`)}`}
+              to={`/video/${video.video_id}?title=${encodeURIComponent(video.video_title)}&thumbnail=${encodeURIComponent(`https://i.ytimg.com/vi/${video.video_id}/mqdefault.jpg`)}`}
               className="video-link"
             >
               <img
-                src={`https://i.ytimg.com/vi/${video.id}/mqdefault.jpg`}
+                src={`https://i.ytimg.com/vi/${video.video_id}/mqdefault.jpg`}
                 alt={video.title}
                 className="video-thumbnail"
               />
               <div className="video-info">
-                <h3 className="video-title">{video.title}</h3>
+                <h3 className="video-title">{video.video_title}</h3>
               </div>
             </Link>
           </div>
